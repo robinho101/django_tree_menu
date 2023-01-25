ae366651e210a65fa0c3d739230624207445a00a
robinho101 <dkdsmg1dh>
2023-01-25 09:16:18 +0300

    final

--- tree_menu/menus/urls.py
+++ tree_menu/menus/urls.py
@@ -4,6 +4,4 @@ from .views import *
 urlpatterns = [
     path('all-menus/', menus, name='menus'),
     path('<int:menu_id>/<str:field_name>/', field, name='field'),
-    path('<int:menu_id>/<str:field_name>/<str:branch_name>/', branch, name='branch'),
-    path('<int:menu_id>/<str:field_name>/<str:branch_name>/<str:nest_name>/', nest, name='nest'),
 ]
